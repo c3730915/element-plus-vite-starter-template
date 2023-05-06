423dcb15651d86386f9516734480dfb1766e5d2a
feifanzhang <1023700418@qq.com>
2023-05-06 18:32:46 +0800

    fix login page not full screen

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,137 @@
-<template>
-  <el-config-provider namespace="ep" style="">
-    <BaseHeader v-if="state.showMenu" style=""/>
-    <BaseSide v-if="state.showMenu"/>
-    <router-view style="display: flex;"/>
-<!--    <div class="flex-container" style="background-color: #42b8dd;display: flex">-->
-<!--    </div>-->
-  </el-config-provider>
-</template>
 
-<script setup>
-// 不需要菜单的路径数组
-import {useRouter} from "vue-router";
-import {reactive} from "vue";
 
-const noMenu = ['/login']
-const router = useRouter()
-const state = reactive({
-  showMenu: true, // 是否需要显示菜单
-})
-// 监听路由的变化
-router.beforeEach((to) => {
-  state.showMenu = !noMenu.includes(to.path)
-})
-</script>
-<style>
-.flex-container {
-  display: flex;
-  min-height: 80vh;
-  /*background-color: #42b8dd;*/
-  border-color: #42b8dd;
-}
+  <template>
+    <el-config-provider namespace="ep" style="">
+    <el-container class="layout-container-demo" style="height: 100vh">
+      <el-aside width="200px" v-if="state.showMenu">
+        <el-scrollbar>
+          <el-menu :default-openeds="['1', '3']">
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><message /></el-icon>Navigator One
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="1-1">Option 1</el-menu-item>
+                <el-menu-item index="1-2">Option 2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="1-3">Option 3</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="1-4">
+                <template #title>Option4</template>
+                <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-sub-menu index="2">
+              <template #title>
+                <el-icon><icon-menu /></el-icon>Navigator Two
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="2-1">Option 1</el-menu-item>
+                <el-menu-item index="2-2">Option 2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="2-3">Option 3</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="2-4">
+                <template #title>Option 4</template>
+                <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-sub-menu index="3">
+              <template #title>
+                <el-icon><setting /></el-icon>Navigator Three
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="3-1">Option 1</el-menu-item>
+                <el-menu-item index="3-2">Option 2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="3-3">Option 3</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="3-4">
+                <template #title>Option 4</template>
+                <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
 
-</style>
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px" v-if="state.showMenu">
+          <div class="toolbar">
+            <el-dropdown>
+              <el-icon style="margin-right: 8px; margin-top: 1px"
+              ><setting
+              /></el-icon>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item>View</el-dropdown-item>
+                  <el-dropdown-item>Add</el-dropdown-item>
+                  <el-dropdown-item>Delete</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+            <span>Tom</span>
+          </div>
+        </el-header>
+
+        <el-main>
+
+          <router-view  style="height: 80vh"></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+    </el-config-provider>
+  </template>
+
+  <script lang="ts" setup>
+  import { ref } from 'vue'
+  import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
+  // 不需要菜单的路径数组
+  import {useRouter} from "vue-router";
+  import {reactive} from "vue";
+
+  const noMenu = ['/login']
+  const router = useRouter()
+  const state = reactive({
+    showMenu: true, // 是否需要显示菜单
+  })
+  // 监听路由的变化
+  router.beforeEach((to) => {
+    state.showMenu = !noMenu.includes(to.path)
+  })
+
+
+
+  </script>
+
+  <style scoped>
+  .layout-container-demo .el-header {
+    position: relative;
+    background-color: var(--el-color-primary-light-7);
+    color: var(--el-text-color-primary);
+    height: 100vh;
+  }
+  .layout-container-demo .el-aside {
+    color: var(--el-text-color-primary);
+    background: var(--el-color-primary-light-8);
+  }
+  .layout-container-demo .el-menu {
+    border-right: none;
+  }
+  .layout-container-demo .el-main {
+    padding: 0;
+  }
+  .layout-container-demo .toolbar {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    right: 20px;
+  }
+  </style>
